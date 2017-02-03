3d0837114a07525c42a57096b053c38dc162b09e
JSCooke <JaydenCooke@outlook.com>
2017-02-03 15:07:47 +1300

    Minimised issues caused by issue #108 for release 2.1.

--- MPAi/Forms/VideoPlayer.cs
+++ MPAi/Forms/VideoPlayer.cs
@@ -130,7 +130,8 @@ namespace MPAi.NewForms
                     List<Recording> videoView = DBModel.Recording.Where(x => (
                        (x.Word.Category.CategoryId == 2)   // If the category is vowel, and
                        && (x.Speaker.SpeakerId == current.Speaker.SpeakerId)    // The speaker matches the current user, and
-                       && ((x.Video != null) || (x.VocalTract != null))   // The recording has a video or vocaltract attached.
+                       && ((x.Video != null))   // There is a video of that speaker, or
+                       || (x.VocalTract != null)   // The recording has a vocaltract attached. (They are gender neutral, albeit with a male voice.)
                        )).ToList();
 
                     wordsList = videoView;   // Take this action before display names are changed
