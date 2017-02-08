614944a563258e442744b1075ad90a4ecd9a7d5e
joshbrun <jbru239@aucklanduni.ac.nz>
2017-02-09 12:49:05 +1300

    Added exception catching to the cleaner.

--- MPAiCleaner/MPAiCleaner/Program.cs
+++ MPAiCleaner/MPAiCleaner/Program.cs
@@ -10,38 +10,44 @@ namespace MPAiCleaner
     {
         static void Main(string[] args)
         {
-
-            Console.Write("MPAi Cleaner:");
-            //Locations of files associated with the MPAi project.
-            Console.Write("Getting appdata location...");
-            Console.Write("Getting temp location...");
-            string mpaiLocation = Path.Combine(Environment.GetEnvironmentVariable("APPDATA"), "MPAi");
-            string tempLocation = Path.Combine(Environment.GetEnvironmentVariable("TEMP"), "MPAiTEMP");
-
-            //Deletes the MPAi directory within the APPDATA path recursively, if it exists.
-            Console.Write("Checking if MPAi exists...");
-
-            if (Directory.Exists(mpaiLocation))
+            try
             {
-                Console.Write("MPAi exists: Deleteing MPAi");
-                Directory.Delete(mpaiLocation, true);
+                Console.WriteLine("MPAi Cleaner:");
+                //Locations of files associated with the MPAi project.
+                string mpaiLocation = Path.Combine(Environment.GetEnvironmentVariable("APPDATA"), "MPAi");
+                string tempLocation = Path.Combine(Environment.GetEnvironmentVariable("TEMP"), "MPAiTEMP");
+                //Deletes the MPAi directory within the APPDATA path recursively, if it exists.
+                if (Directory.Exists(mpaiLocation))
+                {
+                    Console.WriteLine("MPAi exists: Deleteing MPAi");
+                    Directory.Delete(mpaiLocation, true);
+                }
+                else
+                {
+                    Console.WriteLine("MPAi does not exist: Skipping");
+                }
+                //Deletes the MPAiTEMP directory within the TEMP path recursively, if it exists.
+
+                if (Directory.Exists(tempLocation))
+                {
+                    Console.WriteLine("MPAiTEMP exists: Deleteing MPAi");
+                    Directory.Delete(tempLocation, true);
+                }
+                else
+                {
+                    Console.WriteLine("MPAiTEMP does not exist: Skipping");
+                }
+                Console.WriteLine("\nMPAi files have been Cleaned.");
             }
-            else {
-                Console.Write("MPAi does not exist: Skipping");
+            catch (Exception e)
+            {
+                Console.WriteLine("Error cleaning MPAi, MPAi Files Not Cleaned.");
+                Console.WriteLine("You may continue the installation, \nbut it is recommened when the installer begins you cancel the installation.\n and Restart your computer before re running the setup.exe");
+                Console.WriteLine(e.StackTrace);
             }
-
-            //Deletes the MPAiTEMP directory within the TEMP path recursively, if it exists.
-            Console.Write("Checking if MPAiTEMP exists...");
-
-            if (Directory.Exists(tempLocation))
-            {
-                Console.Write("MPAiTEMP exists: Deleteing MPAi");
-
-                Directory.Delete(tempLocation, true);
-            } else
-            {
-                Console.Write("MPAiTEMP does not exist: Skipping");
-
+            finally {
+                Console.WriteLine("Press any key to continue... ");
+                Console.ReadLine();
             }
 
         }
