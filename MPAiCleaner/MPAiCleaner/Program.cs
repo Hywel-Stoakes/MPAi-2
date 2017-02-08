e164e7b39ca40bb19876b73f66697e18ab514f84
joshbrun <joshbrun@users.noreply.github.com>
2017-02-09 12:12:48 +1300

    Adding MPAiCleaner.
    MPAiCleaner is used prior to installing and after uninstalling to purge the users computer of the MPAi files and folders. Forcing the database to be created, and clears the temp files of MPAi.

---
+++ MPAiCleaner/MPAiCleaner/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+namespace MPAiCleaner
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Locations of files associated with the MPAi project.
+            string mpaiLocation = Path.Combine(Environment.GetEnvironmentVariable("APPDATA"), "MPAi");
+            string tempLocation = Path.Combine(Environment.GetEnvironmentVariable("TEMP"), "MPAiTEMP");
+
+            //Deletes the MPAi directory within the APPDATA path recursively, if it exists.
+            if (Directory.Exists(mpaiLocation))
+            {
+                Directory.Delete(mpaiLocation, true);
+            } 
+
+            //Deletes the MPAiTEMP directory within the TEMP path recursively, if it exists.
+            if (Directory.Exists(tempLocation))
+            {
+                Directory.Delete(tempLocation, true);
+            }
+
+        }
+
+    }
+}
